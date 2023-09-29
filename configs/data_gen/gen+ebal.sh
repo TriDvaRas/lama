39dc231b58ffc900bed1a1c2a8b892e243255bf8
TriDvaRas <02dimon02@gmail.com>
2023-09-29 13:49:46 +0300

    fixed paths for colab env

--- configs/data_gen/gen+ebal.sh
+++ configs/data_gen/gen+ebal.sh
@@ -3,8 +3,8 @@ python3 bin/gen_mask_dataset.py $(pwd)/configs/data_gen/thin.yaml my_dataset/eva
 
 python3 bin/gen_mask_dataset.py $(pwd)/configs/data_gen/med.yaml my_dataset/eval3_s/ /mnt/c/Users/User/Desktop/lama/eval/med/in/ --ext jpg
 python3 bin/gen_mask_dataset.py $(pwd)/configs/data_gen/big.yaml my_dataset/eval3_s/ /mnt/c/Users/User/Desktop/lama/eval/big/in/ --ext jpg
-python bin/predict.py model.path=/mnt/o/git/lama3/LaMa_models/big-lama-with-discr/ indir=/mnt/c/Users/User/Desktop/lama/eval/med/in/ outdir=/mnt/c/Users/User/Desktop/lama/eval/med/out_lm/ model.checkpoint=best.ckpt
-python bin/predict.py model.path=/mnt/o/git/lama3/LaMa_models/big-lama-with-discr/ indir=/mnt/c/Users/User/Desktop/lama/eval/big/in/ outdir=/mnt/c/Users/User/Desktop/lama/eval/big/out_lm/ model.checkpoint=best.ckpt
+python bin/predict.py model.path=/content/lama/LaMa_models/big-lama-with-discr/ indir=/mnt/c/Users/User/Desktop/lama/eval/med/in/ outdir=/mnt/c/Users/User/Desktop/lama/eval/med/out_lm/ model.checkpoint=best.ckpt
+python bin/predict.py model.path=/content/lama/LaMa_models/big-lama-with-discr/ indir=/mnt/c/Users/User/Desktop/lama/eval/big/in/ outdir=/mnt/c/Users/User/Desktop/lama/eval/big/out_lm/ model.checkpoint=best.ckpt
 python -m pytorch_fid --batch-size=1 ~/lama/my_dataset/train/ /mnt/c/Users/User/Desktop/lama/eval/med/out_lm/
 python -m pytorch_fid --batch-size=1 ~/lama/my_dataset/train/ /mnt/c/Users/User/Desktop/lama/eval/big/out_lm/
 
